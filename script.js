0ac7e145c68d4065e199309482d0c7064d353980
HUELLAS.E67 <jonsesjfarrieta2008@gmail.com>
2024-10-26 11:52:21 -0400

    xd

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 console.log("hola mundo!")
 
-for(let sum = 1; sum <= 20; sum++){
-    
-    if (sum % 2 === 0)
-        console.log(sum+"par")
-    else{
-        console.log(sum+"impar")
-    }
 
+function sumarnumeros(){
+    for(let sum = 1; sum <= 20; sum++){
+    
+        if (sum % 2 === 0)
+        console.log(sum+"par")
+        else{
+        console.log(sum+"impar")
+        }
+
+    }
 }
