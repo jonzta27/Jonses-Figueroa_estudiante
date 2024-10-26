269cb678db0498a30aac98e2901a7531e647228d
HUELLAS.E67 <jonsesjfarrieta2008@gmail.com>
2024-10-26 11:32:02 -0400

    cambie el script

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hola mundo")
+console.log("hola mundo!")
+
+for(let sum = 1; sum <= 20; sum++){
+    
+    if (sum % 2 === 0)
+        console.log(sum+"par")
+    else{
+        console.log(sum+"impar")
+    }
+
+}
